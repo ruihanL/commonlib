e1819304b36b8397eaa242e0676e25d44191b765
ruihanL <ruihanL@126.com>
2019-07-04 13:38:37 +0800

    d

---
+++ app/src/main/java/com/linrh/commonlibs/MainApp.java
@@ -0,0 +1,19 @@
+package com.linrh.commonlibs;
+
+import android.app.Application;
+
+/**
+ * 作者：created by @author{ John } on 2019/7/4 0004上午 10:32
+ * 描述：
+ * 修改备注：
+ */
+
+public class MainApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
